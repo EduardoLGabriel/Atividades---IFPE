83856e56ddc29c68337dfd071eb553c0ad3ece1f
EduardoLGabriel <eduardo.l2006.gab23@gmail.com>
2026-09-14 20:43:00 -0300

    Update paciente.java
    troca de char genero para string genero

--- clinica/src/main/java/com/clinica/clinica/paciente.java
+++ clinica/src/main/java/com/clinica/clinica/paciente.java
@@ -6,12 +6,12 @@ public class paciente {
 		private String nomePaciente;
 		private String CPF;
 		private int idade;
-		private char genero;
+		private String genero;
 		private ArrayList<String> Alergias;
 		private String observacoes;
 		
 		
-		public paciente(int ID, String nomePaciente, String CPF, int idade, char genero, String observacoes) {
+		public paciente(int ID, String nomePaciente, String CPF, int idade, String genero, String observacoes) {
 			this.nomePaciente=nomePaciente;
 			this.CPF=CPF;
 			this.idade = idade;
